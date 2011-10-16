f0d11dcb3d430760a754c0dfa9eda643a4fbead6
Steven Klise <steven@stevenklise.com>
2011-10-16 17:40:57 -0400

    Documentation helper to generate s3 anchor

--- app/models/documentation.rb
+++ app/models/documentation.rb
@@ -2,16 +2,19 @@ class Documentation < ActiveRecord::Base
 
   belongs_to :thesis
 
-  validates_uniqueness_of :aws_url
-
   has_attached_file :media, 
                     :storage => :s3, 
                     :s3_credentials => "#{Rails.root}/config/s3_credentials.yml",
+                    :s3_permissions => :public_read,
                     :path => "/documentation/:id/:filename"
 
   def condition_word
     words = { 1 => "Unusable", 2 => "Fragment", 3 => "Good", 4 => "Perfect"}
     words[condition]
+  end
+
+  def link_to_pdf(text)
+    "<a href='http://s3.amazonaws.com/itp_archive#{media.path}'>#{text}</a>".html_safe
   end
 
 end
--- db/migrate/20110923170348_create_documentations.rb
+++ db/migrate/20110923170348_create_documentations.rb
@@ -2,7 +2,6 @@ class CreateDocumentations < ActiveRecord::Migration
   def change
     create_table :documentations do |t|
       t.string :type
-      t.string :aws_url
       t.integer :condition
       t.integer :thesis_id
       t.string :title
