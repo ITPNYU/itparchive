ed9a67a0df74cac937529edeec935f39482a6521
Steven Klise <steven@stevenklise.com>
2011-10-19 18:08:01 -0400

    Prepopulate forms for edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     options = [:hard_wrap,  :autolink, :fenced_code, :gh_blockcode]
     Redcarpet.new(text, *options).to_html.html_safe
   end
+
+  def edit_value(model, field)
+    "value='#{model[field]}'" if model
+  end
 end
