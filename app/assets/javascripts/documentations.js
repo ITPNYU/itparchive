e18c81b7254b7cb9a0a0bc3f3faf6e5b919ccb42
Steven Klise <steven@stevenklise.com>
2011-10-22 13:57:56 -0400

    Form post works

--- app/assets/javascripts/documentations.js
+++ app/assets/javascripts/documentations.js
@@ -4,10 +4,28 @@ $(function(){
   });
   
   $('.add-thesis-form .submit').click(function(){
-    $formdata = $(this).closest('.add-thesis-form')
+    $theform = $(this).closest('.add-thesis-form');
+    form = {
+      id : $('input[name="documentation"]').val(),
+      thesis : {
+        title : $('input[name="thesis[title]"]').val(),
+        year : $('input[name="thesis[year]"]').val(),
+        notes : $('input[name="thesis[notes]"]').val(),
+      },
+      person : {
+        first : $('input[name="person[first]"]').val(),
+        last : $('input[name="person[last]"]').val(),
+        year : $('input[name="person[year]"]').val()
+      }
+    }
     $.ajax({
-      url: '/theses/create',
-      type: 'POST'
+      url : '/documentations/'+form["id"],
+      type : 'PUT',
+      data : form,
+      success : function(response){
+        $theform.empty();
+        $theform.html("<h5>Thesis created!</h5>");
+      }
     });
     event.preventDefault();
     return false;
