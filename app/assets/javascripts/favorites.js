72d84f049aba218799f4b06c7a94ec7a9c032a65
Steven Klise <steven@stevenklise.com>
2011-11-13 16:12:35 -0500

    Favorite Helper and jQuery handlers

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -1,11 +1,20 @@
 $(function(){
-  $('.add-favorite').click(function(){
-    event.preventDefault();
+  $('[class="favorite-star"][class!="favorited"]').click(function(){
+    $(this)
+      .addClass('favorited')
+      .addClass('has-favorites')
+      .find('.star').text("★");
+    $('<li class="current-user">You</li>').appendTo($(this).find('.favorites-list'));
     $.ajax({
       url : '/favorites',
       type : 'POST',
-      data : {thesis_id : $(this).attr('href').substring(1)}
+      data : {thesis_id : $(this).attr('href').substring(1)},
+      
     });
+    return false;
+  });
+
+  $('.favorited').click(function(){
     return false;
   });
 });
