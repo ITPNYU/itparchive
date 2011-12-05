91f3de71eddbb2cccea6b7460aad3937b9e9a343
Steven Klise <steven@stevenklise.com>
2011-12-05 15:40:11 -0500

    Fix conditions syntax from is to = in Note.find

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   end
 
   def dashboard
-    @your_notes = Note.find(:all, :order => ["created_at DESC"], :conditions => ["user_id is #{current_user.id}"])
+    @your_notes = Note.find(:all, :order => ["created_at DESC"], :conditions => ["user_id = ?", current_user.id])
     @recent_favorites = Favorite.recent
     @recently_read = Read.recent
     @recent_notes = Note.find(:all, :order => ["created_at DESC"], :limit => 10, :include => [:user, :notable])
