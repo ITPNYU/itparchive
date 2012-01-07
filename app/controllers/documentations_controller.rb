2a75f24dc534c05ec19d6a6bdb2c3430f9834e7c
Steven Klise <steven@stevenklise.com>
2012-01-07 15:03:35 -0500

    Comment out cache

--- app/controllers/documentations_controller.rb
+++ app/controllers/documentations_controller.rb
@@ -1,6 +1,6 @@
 class DocumentationsController < ApplicationController
   before_filter :authenticate_user!, :except => [:show, :index]
-  caches_action :index
+  # caches_action :index
 
   def index
     @documentations = Documentation.where(:thesis_id => nil)
@@ -14,36 +14,34 @@ class DocumentationsController < ApplicationController
     @documentation = Documentation.find(params[:id])
     @documentation.update_attributes({
       :physical_location => params[:physical_location],
+      :read => params[:read],
       :has_images => params[:has_images],
       :integrity => params[:integrity]
     })
 
     if params[:read] == "true"
-      if @documentation.reads.find(:first, :conditions => ["user_id = #{current_user.id}"]).nil?
-        @documentation.reads << Read.new(:user_id => current_user.id)
+      if @documentations.reads.find(:first, :conditions => ["user_id is #{current_user.id}"]).nil?
+        @documentations.reads << Read.new(:user_id => current_user.id)
       end
     else
-      if r = @documentation.reads.find(:first, :conditions => ["user_id = #{current_user.id}"])
+      if r = @documentations.reads.find(:first, :conditions => ["user_id is #{current_user.id}"])
         r.destroy
       end
     end
 
+    @documentation.integrity = params[:integrity]
     if (params[:flag] == "true")
       @documentation.flag = true
     else
       @documentation.flag = false
     end
-
-    @documentation.thesis = Thesis.new({
-      title: params[:thesis][:title],
-      year: params[:thesis][:year],
-      person: Person.new(params[:person]),
-    })
-    @documentation.thesis.save
+    @documentation.thesis = Thesis.new(params[:thesis])
+    @documentation.thesis.person = Person.new(params[:person])
 
     respond_to do |format|
       if @documentation.save
-        format.all { render json: @documentation}
+        format.html { redirect_to "/documentations", notice: 'Thesis was successfully updated.' }
+        format.json { head :ok }
       else
         format.html { render action: "edit" }
         format.json { render json: @documentation.errors, status: :unprocessable_entity }
