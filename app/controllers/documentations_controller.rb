1d32c12437f1f7e2b31461b0666e24aeda3cbcdc
Steven Klise <steven@stevenklise.com>
2012-01-07 15:33:05 -0500

    pg syntax

--- app/controllers/documentations_controller.rb
+++ app/controllers/documentations_controller.rb
@@ -19,11 +19,11 @@ class DocumentationsController < ApplicationController
     })
 
     if params[:read] == "true"
-      if @documentation.reads.find(:first, :conditions => ["user_id is #{current_user.id}"]).nil?
+      if @documentation.reads.find(:first, :conditions => ["user_id = #{current_user.id}"]).nil?
         @documentation.reads << Read.new(:user_id => current_user.id)
       end
     else
-      if r = @documentation.reads.find(:first, :conditions => ["user_id is #{current_user.id}"])
+      if r = @documentation.reads.find(:first, :conditions => ["user_id = #{current_user.id}"])
         r.destroy
       end
     end
