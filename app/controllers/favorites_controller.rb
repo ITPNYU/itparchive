d1260e94d8e78d09e4a4f2ed44e679c73dd5f28c
Steven Klise <steven@stevenklise.com>
2012-01-18 12:10:28 -0500

    Add destroy method for Favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,6 +5,16 @@ class FavoritesController < ApplicationController
     @favorites = Favorite.all
   end
 
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to favorites_url }
+      format.json { head :ok }
+    end
+  end
+
   def create
     @favorite = Favorite.new({
       :user_id => current_user.id,
