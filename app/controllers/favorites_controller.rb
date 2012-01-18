51f25fec547663bd6cbf5b42e6d7f204fbac7aa8
Steven Klise <steven@stevenklise.com>
2012-01-17 19:32:20 -0500

    Hide Favorites from unauthenticated visitors

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,5 @@
 class FavoritesController < ApplicationController
-  before_filter :authenticate_user!, :except => [:show, :index]
+  before_filter :authenticate_user!
 
   def index
     @favorites = Favorite.all
