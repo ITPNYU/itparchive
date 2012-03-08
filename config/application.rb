5b5c698939fce798db1592ee54f8916faec9438e
Steven Klise <sklise@gmail.com>
2012-03-08 13:22:58 -0500

    Prevent application startup on asset:precompile

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Itparchive
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Prevent initializing application during asset precompile
+    config.assets.initialize_on_precompile = false
   end
 end
